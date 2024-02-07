360d718be94add1058ecc8ec489b277675c1d58f
Zweidalos95 <159389364+Zweidalos95@users.noreply.github.com>
2024-02-07 17:18:01 -0600

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,12 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 theta=np.linspace(0,2*np.pi,100)
 for ang in theta:
 	print(np.cos(ang),np.sin(ang))
 
 
-plt.plot(np.cos(theta),np.sin(theta))
-plt.savefig('Try.jpg')
+#plt.plot(np.cos(theta),np.sin(theta))
+#plt.savefig('Try.jpg')
 
 plt.show()
